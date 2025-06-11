42d1c18b5bd93efcb184622e9f9150892b18a035
Jared Pinfold <j.pinfold@gmail.com>
2025-06-11 16:49:25 +1200

    tutorial part 2 complete

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello from Electron 👋')
+import { app, BrowserWindow } from 'electron'
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
