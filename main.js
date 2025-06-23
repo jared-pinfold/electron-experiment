c5071ea32ab2d7247f0d229e5eff9d8ea8f326bd
Jared Pinfold <j.pinfold@gmail.com>
2025-06-24 09:16:30 +1200

    fixed preload functionality

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow } from "electron";
+import { installExtension, REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+import path from "node:path";
 
-function createWindow () {
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+    },
+  });
 
-  win.loadFile('index.html')
+  win.loadFile("index.html");
+  win.webContents.openDevTools()
 }
 
+///// Opens window when app loads
 app.whenReady().then(() => {
-  createWindow()
-})
+  installExtension(REACT_DEVELOPER_TOOLS)
+        .then((ext) => console.log(`Added Extension:  ${ext.name}`))
+        .catch((err) => console.log('An error occurred: ', err));
+  createWindow();
+
+  app.on("activate", () => {
+    // for macOS as app runs in the background if window is closed
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+///// Quits the app when all windows are closed, except on macOS
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ renderer.js
@@ -0,0 +1,4 @@
+const information = document.getElementById("info");
+information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`;
+// information.innerText = 'helooooo'
+// information.innerText = `This app is using Chrome (v${"poo"}), Node.js (v${"yay"}), and Electron (v${'w'})`;
